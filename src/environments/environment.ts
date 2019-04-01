13132c4055ca085a7cf9b5ae335830581d68b580
sirrotich <rotichtitus12@gmail.com>
2019-04-01 21:24:00 +0300

    build the work environment by having the api to the apiUrl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,6 +7,7 @@ export const environment = {
   apiUrl: 'bf382007a8b42c5cae2c6ba3506e2bc40f880919'
 };
 
+
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
